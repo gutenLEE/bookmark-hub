b65af115c5ecd6bce2b8834b13d736238fa9e9b2
gutenLEE <3bnm1128@naver.com>
2022-11-15 23:36:17 +0900

    feat: oauth  개발중

--- scripts/authorize.js
+++ scripts/authorize.js
@@ -1,7 +1,10 @@
-import { Octokit, App } from "octokit";
+import { OAuthApp, createNodeMiddleware } from "octokit";
 
-const octokit = new Octokit({
-    auth: ProcessingInstruction.env.TOKEN,
-});
 
-await octokit.request("GET /octocat", {})
+const { url } = app.getWebFlowAuthorizationUrl({
+    clientType: "oauth-app",
+    clientId: "be7364bd92671966625b",
+    clientSecret: "6b5d6da12cd72baac6eb7db78ef74de0231d5cab",
+    defaultScopes: ["repo"],
+})
+console.log(url)
--- scripts/oauth2.js
+++ scripts/oauth2.js
@@ -1,4 +1,5 @@
 const oAuth2 = {
+
     init() {
         this.KEY = 'bookmark-hub-token';
         this.ACCESS_TOKEN_URL =
@@ -9,6 +10,17 @@ const oAuth2 = {
         this.CLIENT_SECRET = '6b5d6da12cd72baac6eb7db78ef74de0231d5cab';
         this.REDIRECT_URL = 'https://github.com/'; 
         this.SCOPES = ['repo'];
+    },
+
+    begin() {
+        this.init();
+        
+        let url = `${this.AUTHORIZATION_URL}?client_id=${this.CLIENT_ID}&redirect_uri${this.REDIRECT_URL}&scope=`;
+
+        for (let i = 0; i < this.SCOPES.length; i += 1) {
+            url += this.SCOPES[i];
+        }
+        console.log(url)
     }
 
 }
